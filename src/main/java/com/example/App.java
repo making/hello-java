927720cd263bb08bceb38aae0d8423198a52940a
Toshiaki Maki <tmaki@pivotal.io>
2018-06-19 16:49:56 +0900

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,6 +6,5 @@ public class App {
     public static void main(String[] args) {
         Spark.port(8080);
         Spark.get("/", (req, res) -> "Hello World!");
-        Runtime.getRuntime().addShutdownHook(new Thread(Spark::stop));
     }
 }
